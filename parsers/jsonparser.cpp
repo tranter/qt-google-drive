da0c9817940529e2ec5decb6481e29559ce533bf
Oleg Ziakun <oziakun@ics.com>
2012-12-17 10:39:03 +0200

    JSONParser::getPlainParam parser refactoring

--- parsers/jsonparser.cpp
+++ parsers/jsonparser.cpp
@@ -10,30 +10,29 @@ JSONParser::JSONParser()
 
 QString JSONParser::getPlainParam(const QString& jsonStr, const QString& lVal)
 {
-    QString optStr = jsonStr;
+    QString optStr(jsonStr);
 
     optStr.remove(QRegExp("[{}\"\n]"));
 
-    QStringList parseStrs = optStr.split(",");
-    QString rVal;
+    QStringList parseStrs(optStr.split(","));
+    QStringList token;
+    QString rVal, token0, token1;
 
     for (int i = 0; i < parseStrs.count(); ++i)
     {
-           QStringList token = parseStrs[i].split(": ");
+        token = parseStrs[i].split(": ");
 
-            if(token.count() == 2)
+        if(token.count() == 2)
+        {
+            token0 = token[0].trimmed();
+            token1 = token[1].trimmed();
+
+            if(token0 == lVal)
             {
-                QString token0, token1;
-
-                token0 = token[0].trimmed();
-                token1 = token[1].trimmed();
-
-                if(token0 == lVal)
-                {
-                    rVal = token1;
-                    break;
-                }
+                rVal = token1;
+                break;
             }
+        }
     }
 
     return rVal;
